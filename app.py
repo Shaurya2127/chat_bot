920c0bf21415d99f8c4267cec11c04f80c370ba0
Shaurya Chauhan <150056928+Shaurya2127@users.noreply.github.com>
2025-04-27 20:39:10 +0530

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ classes = pickle.load(open("classes.pkl", "rb"))
 
 # Functions
 def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = sentence.lower().split() 
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
